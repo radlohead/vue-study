114c23f09e4b29a7acc9a589283e029cc93b9a92
minho <radlohead@naver.com>
2019-05-08 20:34:19 +0900

    v-bind, v-html add

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <h3>{{ title }}</h3>
+    <a v-bind:href="testUrl">test link</a>
+    <p v-html="testUrlLink"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+      title: "asdfadfs",
+      testUrl: "https://www.naver.com",
+      testUrlLink: "<a href='https://www.naver.com'>test url link</a>"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
