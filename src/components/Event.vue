0b1ac2360808ac7af7cef4df52abe3f81eda3501
minho <radlohead@naver.com>
2019-05-12 13:47:40 +0900

    지역 컴포넌트 등록

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -3,15 +3,23 @@
     wrapper
     <h1>{{ title }}</h1>
     <h2>{{ num }}</h2>
+    <my-component></my-component>
   </section>
 </template>
 
 <script>
+const Child = {
+  template: "<div>지역 컴포넌트 입니다.</div>"
+};
+
 export default {
   name: "Event",
   props: {
     title: String,
     num: String
+  },
+  components: {
+    "my-component": Child
   }
 };
 </script>
